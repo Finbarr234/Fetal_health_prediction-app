5eb141a410c8c2aa280d332e8cada08cb95b3bf3
Finbarr Ekenedilichukwu okonkwo <141606316+Finbarr234@users.noreply.github.com>
2026-06-12 15:26:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,4 @@ import pandas as pd
 st.title('🎈 Fetal_health_prediction-app')
 
 st.info('This is a featl health prediction app!')
-df * pd.read_csv('https://github.com/Finbarr234/Fetal_health_prediction-app/blob/master/fetalhealth%20(1).csv')
-
+df = pd.read_csv("https://raw.githubusercontent.com/Finbarr234/Fetal_health_prediction-app/master/fetalhealth%20(1).csv")
